72495bf2326a5b211ad66cbf88c0409fa1ea690a
fritz-bautista <evodark93@gmail.com>
2026-08-21 12:53:23 +0800

    refactor: remove unused Footer import from Home component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,6 @@ import About from '../components/About.tsx';
 import Contact from '../components/Contact.tsx';
 import History from '../components/History.tsx';
 import Projects from '../components/Projects.tsx';
-import Footer from '../components/Footer.tsx';
 import TextMarquee from '../components/TextMarquee.tsx';
 import VideoPresentation from '../components/VideoPresentation.tsx';
 
