363d9d2768c0b691244c61d66e98321f04844d54
Fritz Harold <evodark93@gmail.com>
2026-09-19 17:34:44 +0800

    feat: update Hero component layout and styling; enhance BlurIn text size and responsiveness

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -27,18 +27,22 @@ function Hero() {
   }
 
   return (
-    <div className="flex flex-col bg-black items-center justify-center w-full h-full">
+    <div className="flex flex-col bg-black items-center justify-center w-full h-screen">
       <motion.div 
       variants={containerVariants}
       initial="hidden"
       animate="show"
-      className="grid grid-cols-5 grid-rows-5 gap-0.5  w-full h-screen items-center justify-center">
+      className="grid grid-cols-5 grid-rows-5 gap-0.5 flex w-full h-fulll items-center justify-center">
           <motion.div variants={itemVariants} className="row-span-2 bg-white w-full h-full"></motion.div>
           <motion.div variants={itemVariants} className="col-span-3 row-span-2 bg-white w-full h-full"></motion.div>
           <motion.div variants={itemVariants} className="row-span-2 col-start-5 bg-white w-full h-full"></motion.div>
-          <motion.div variants={itemVariants} className="col-span-3 col-start-2 row-start-3 bg-white w-full h-full pl-30 pr-30">
-            <BlurIn>FRITZ PORTFOLIO</BlurIn>
-              <WordsPullUp text="This is a collection of my Graphic design, UI design, and Website application projects and commissions."/>
+          <motion.div variants={itemVariants} className="col-span-3 col-start-2 row-start-3 flex flex-col bg-white w-full h-full">
+            <div className="grid grid-cols-1 grid-rows-2 flex p-10 items-center justify-center">
+              <BlurIn>FRITZ PORTFOLIO</BlurIn>
+              <div className="w-full pl-15 pr-15">
+                <WordsPullUp text="This is a collection of my Graphic design, UI design, and Website application projects and commissions."/>
+              </div>
+            </div>
           </motion.div>
           <motion.div variants={itemVariants} className="col-start-1 row-start-3 bg-white w-full h-full"></motion.div>
           <motion.div variants={itemVariants} className="col-start-5 row-start-3 bg-white w-full h-full"></motion.div>
--- src/components/TextComponents/Blur_in.tsx
+++ src/components/TextComponents/Blur_in.tsx
@@ -11,7 +11,7 @@ export const BlurIn = ({ children }: { children: React.ReactNode }) => {
       initial={{ filter: 'blur(20px)', opacity: 0 }}
       animate={isInView ? { filter: 'blur(0px)', opacity: 1 } : {}}
       transition={{ duration: 1.2 }}
-      className="text-xl text-center sm:text-4xl font-google font-bold tracking-tighter md:text-8xl md:leading-[7rem]"
+      className="text-sm md:text-base lg:text-2xl xl:text-7xl text-center font-google font-bold tracking-tighter md:text-8xl md:leading-[5rem]"
     >
       {children}
     </motion.h2>
